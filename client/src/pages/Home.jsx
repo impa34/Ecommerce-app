59516efd27eecbd6224aa9ac5478263e73bddcb9
impa34 <81638823+impa34@users.noreply.github.com>
2025-06-11 17:12:56 +0200

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,8 +2,8 @@
 function Home() {
 
   return (
-    <div className="bg-white text-black">
-       <h2>Welcome Guest!</h2>
+    <div className="bg-white text-black flex justify-center mt-5">
+       <h2 className="text-3xl font-bold">Welcome Guest!</h2>
     </div>
   )
 }
